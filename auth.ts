2e974e22973763844c960044830bd05419372a9a
Antonio André <antonioandre1008@gmail.com>
2024-09-19 08:56:26 -0500

    feat(provider): visualiza log

--- auth.ts
+++ auth.ts
@@ -1,6 +1,15 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [Google],
+export const {handlers, signIn, signOut, auth} = NextAuth({
+  providers: [Google],
+  callbacks: {
+    async signIn({account, profile}) {
+      if (account.provider === "google") {
+        console.log("Google account", account)
+        return true
+      }
+      return true // Do different verification for other providers that don't have `email_verified`
+    },
+  },
 })
