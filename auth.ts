9cb08d7ce5a80cf37e68c9445a0beac3f216a04f
Antonio André <antonioandre1008@gmail.com>
2024-09-19 20:40:12 -0500

    chore: configura prisma

--- auth.ts
+++ auth.ts
@@ -1,19 +1,15 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import {PrismaAdapter} from "@auth/prisma-adapter";
+import {prisma} from "@/prisma";
 
-type Profile = {
-  name?: string;
-  email?: string;
-  image?: string;
-  role?: string; // Add role field
-};
 
 export const {handlers, signIn, signOut, auth} = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Google],
   callbacks: {
     async signIn({account, profile}) {
       if (account && account.provider === "google") {
-        profile.role = account.role; // Assign role based on user's choice
         console.log("Google account", account);
         console.log("Google profile", profile);
         return true;
