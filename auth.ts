1d899c789693b54bfbdf705819ff26c689a43bea
Antonio André <antonioandre1008@gmail.com>
2024-09-19 21:57:37 -0500

    test: up

--- auth.ts
+++ auth.ts
@@ -1,12 +1,12 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import {PrismaAdapter} from "@auth/prisma-adapter";
-import {prisma} from "@/prisma";
-
+import {prisma} from "@/app/lib/db";
 
 export const {handlers, signIn, signOut, auth} = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
+  trustHost: true,
   callbacks: {
     async signIn({account, profile}) {
       if (account && account.provider === "google") {
